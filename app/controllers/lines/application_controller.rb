86f5ae16f3db1363417780bfb40c12827378676b
Jochen Greif <jochen@opoloo.de>
2014-10-01 00:19:38 +0200

    correct namespacing for Lines::User model

--- app/controllers/lines/application_controller.rb
+++ app/controllers/lines/application_controller.rb
@@ -5,7 +5,7 @@ module Lines
 
       # sets the current_lines_user if one exists in session
       def current_lines_user
-        @current_lines_user ||= User.find(session[:user_id]) if session[:user_id]
+        @current_lines_user ||= Lines::User.find(session[:user_id]) if session[:user_id]
       end
       helper_method :current_lines_user
 
