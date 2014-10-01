8e0fbf25abb6850d88e35afd126b8c9e38ca7981
Jochen Greif <jochen@opoloo.de>
2014-10-01 18:58:20 +0200

    FIX: user strong parameters for updating authors

--- app/controllers/lines/admin/authors_controller.rb
+++ app/controllers/lines/admin/authors_controller.rb
@@ -41,7 +41,7 @@ module Lines
       def update
         @author = Author.find(params[:id])
 
-        if @author.update_attributes(params[:author])
+        if @author.update_attributes(author_params)
           redirect_to admin_author_path @author, notice: 'Author was successfully updated.'
         else
           render action: "edit"
