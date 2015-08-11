4bcc64cf521193d88ec6a67cc5cc03da881829bf
Jochen Greif <jochen@opoloo.de>
2015-08-11 09:17:58 +0200

    removed migrations from test dummy

--- spec/dummy/db/migrate/20141027143656_add_teaser_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddTeaserToArticle < ActiveRecord::Migration
-  def change
-    add_column :lines_articles, :teaser, :text
-  end
-end
--- spec/dummy/db/migrate/20150421093311_add_reset_password_fields_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddResetPasswordFieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :lines_users, :reset_digest, :string
-    add_column :lines_users, :reset_sent_at, :datetime
-  end
-end
