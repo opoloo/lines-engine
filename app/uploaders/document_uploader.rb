02498709e8d82e95f62a989fe6b15d6cd90267a2
Adones Pitogo <pitogo.adones@gmail.com>
2018-11-26 04:39:27 +0800

    using ftp

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -2,7 +2,7 @@
 # Carrierwave uploader for docuemnts.
 class DocumentUploader < CarrierWave::Uploader::Base
 
-  storage :file
+  storage :ftp
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
