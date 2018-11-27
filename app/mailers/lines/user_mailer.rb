17d1236eb3d8ac5fbcf85ce1d704d7627c6d8bdf
Me <jochen@opoloo.de>
2018-11-27 12:14:01 +0100

    Use configured email-address for current Rails.env

--- app/mailers/lines/user_mailer.rb
+++ app/mailers/lines/user_mailer.rb
@@ -1,6 +1,6 @@
 module Lines
   class UserMailer < ActionMailer::Base
-    default from: CONFIG[:production]['from_email']
+    default from: CONFIG[Rails.env.to_sym]['from_email']
     
     def password_reset(user)
       @user = user
